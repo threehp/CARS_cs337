273436d09012fdc46269dd7d5ad66ec8490f3650
3hp <threehp@users.noreply.github.com>
2017-10-22 04:42:32 -0700

    added 2 dummy pages

--- src/CARSPackage/CARS_LogIn.java
+++ src/CARSPackage/CARS_LogIn.java
@@ -14,9 +14,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import classes.students;
+
 
 /**
  * Servlet implementation class CARS_LogIn
